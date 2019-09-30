d650ede3e7a12cbabce1ea382ebe7d32ded8544c
Wilson Ho <wilson.ho2718@gmail.com>
2019-09-30 13:12:49 -0700

    REEEEeeeee

--- index.js
+++ index.js
@@ -135,84 +135,81 @@ function sendMessage (recipientId, message) {
     });
 }*/
 
+
 'use strict';
 
 const
-    request = require('request'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    app = express().use(bodyParser.json());
-
-const mongoose = require('mongoose');
-var db = mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
-var Member = require('./models/member');
+  request = require('request'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json());
 
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 app.post('/webhook', (req, res) => {
-    let body = req.body;
+  let body = req.body;
 
-    if (body.object === 'page') {
-        body.entry.forEach(function(entry) {
-            entry.messaging.forEach(function(event) {
-                if (event.message)
-                    processMessage(event);
-            });
-        });
-        res.status(200).send('EVENT_RECEIVED');
-    }
-    else {
-        res.sendStatus(404);
-    }
+  if (body.object === 'page') {
+      body.entry.forEach(function(entry) {
+          entry.messaging.forEach(function(event) {
+              if (event.message)
+                  processMessage(event);
+          });
+      });
+      res.status(200).send('EVENT_RECEIVED');
+  }
+  else {
+      res.sendStatus(404);
+  }
 });
 
 app.get("/", function (req, res) {
-  res.send("Deployed!");
+res.send("Deployed!");
 });
 
 app.get('/webhook', (req, res) => {
-    const VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
+  const VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
 
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
 
-    if (mode && token) {
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-        }
-        else {
-            res.sendStatus(403);
-        }
-    }
+  if (mode && token) {
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+          console.log('WEBHOOK_VERIFIED');
+          res.status(200).send(challenge);
+      }
+      else {
+          res.sendStatus(403);
+      }
+  }
 });
 
 function processMessage (event) {
-    if (!event.message.is_echo) {
-        var senderId = event.sender.id;
-        var message = event.message;
-        var sent = event.timestamp;
+  if (!event.message.is_echo) {
+      var senderId = event.sender.id;
+      var message = event.message;
+      var sent = event.timestamp;
 
-        console.log("Received message from senderId: " + senderId);
-        console.log("Message is: " + JSON.stringify(message));
-        console.log("Message sent at: " + sent);
+      console.log("Received message from senderId: " + senderId);
+      console.log("Message is: " + JSON.stringify(message));
+      console.log("Message sent at: " + sent);
 
-        sendMessage(senderId, {text: "test"});
-    }
+      sendMessage(senderId, {text: "test"});
+  }
 }
 
 function sendMessage (recipientId, message) {
-    request({
-        url: "https://graph.facebook.com/v2.6/me/messages",
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: "POST",
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function (err, response, body) {
-        if (err)
-            console.log("Error sending messages: " + err);
-    });
+  request({
+      url: "https://graph.facebook.com/v2.6/me/messages",
+      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+      method: "POST",
+      json: {
+          recipient: {id: recipientId},
+          message: message,
+      }
+  }, function (err, response, body) {
+      if (err)
+          console.log("Error sending messages: " + err);
+  });
 }

f71efb35776c7921502530fe4fc7730254f9139a
Wilson Ho <wilson.ho2718@gmail.com>
2019-09-29 17:46:34 -0700

    Update schema

--- models/member.js
+++ models/member.js
@@ -3,7 +3,10 @@ var Schema = mongoose.Schema;
 
 var MemberSchema = new Schema({
     user_id: {type: String, unique: true},
-    name: String
+    name: String,
+    email: String,
+    grade: Number,
+    points: Number
 });
 
 module.exports = mongoose.model("Member", MemberSchema);

612d744c7d4522a136d6baea72f5acaae7bcfbe1
Wilson Ho <wilson.ho2718@gmail.com>
2019-10-01 23:36:15 -0700

    Re

--- sheets.js
+++ sheets.js
@@ -63,7 +63,6 @@ function updateSheet(auth) {
             console.log(err);
         else {
             const rows = res.data.values;
-            console.log(rows);
             if (!rows)
                 console.log("No data found");
             else {
@@ -142,7 +141,6 @@ function updateSheet(auth) {
             console.log(errA);
         else {
             var mObj = JSON.parse(JSON.stringify(docsA));
-            console.log(mObj+ " " + mObj.length);
             if (mObj.length) {
                 const data = mObj.map(row => Object.values(row));
                 const resource = {

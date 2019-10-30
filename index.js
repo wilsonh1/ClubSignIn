891dc73cad85dd15f71506b14961427f0da8fada
Wilson Ho <wilson.ho2718@gmail.com>
2019-10-29 22:31:09 -0700

    Update

--- index.js
+++ index.js
@@ -61,8 +61,9 @@ function processMessage (event) {
         console.log("Message is: " + JSON.stringify(message));
         console.log("Message sent at: " + sent);
 
+        const notRec = "Message not recognized. Send \"help\" for a list of valid messages.";
         if (!message.text)
-            sendMessage(senderId, {text: "Message not recognized. Send \"help\" for a list of valid messages."});
+            sendMessage(senderId, {text: notRec});
         else {
             var str = message.text.toLowerCase().split(" ");
             if (str[0] == "sign" && str[1] == "in") {
@@ -77,7 +78,7 @@ function processMessage (event) {
                 else if (str[1] == "grade")
                     updateGrade(senderId, str[2]);
                 else
-                    sendMessage(senderId, {text: "Message not recognized. Send \"help\" for a list of valid messages."});
+                    sendMessage(senderId, {text: notRec});
             }
             else if (str[0] == "check")
                 getField(senderId, str[1]);
@@ -90,7 +91,7 @@ function processMessage (event) {
                 sendMessage(senderId, {text: "check grade"});
             }
             else
-                sendMessage(senderId, {text: "Message not recognized. Send \"help\" for a list of valid messages."});
+                sendMessage(senderId, {text: notRec});
         }
     }
 }

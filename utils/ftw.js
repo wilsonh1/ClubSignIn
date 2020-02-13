88f0f44b57cc9d5404d484c9d605926c924947f4
Wilson Ho <wilson.ho2718@gmail.com>
2020-02-13 02:03:09 -0800

    Test

--- utils/ftw.js
+++ utils/ftw.js
@@ -16,6 +16,7 @@ function checkInGame (senderId, flag, callback, id = []) {
         if (errU)
             console.log(errU);
         else {
+            console.log(uObj['game_id']);
             var inGame = (uObj && uObj['game_id'] && uObj['game_id'] != -1 && id.indexOf(uObj['game_id']) == -1);
             if (flag) {
                 if (inGame)
